3f162e6c0ec435c97bbf08e86454fc289325cec1
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-24 15:33:53 +0900

    DBs1

--- src/dbload.java
+++ src/dbload.java
@@ -1,13 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Ashley
- */
+import java.io.File;
+import java.io.IOException;
+
+
 public class dbload {
-    
+    public static void main (String args[])
+            throws IOException {
+        if(args[0].equals("-p")) {
+            if (args.length!=3){
+                System.err.println("Please correct parameter");
+                return;
+            }
+            File source_File=new File(args[2]);
+            File target_File=new File("heap." + args[1]);
+            buildHeapFile(source_File,target_File, Integer.parseInt(args[1]));
+        }
+    }
+
+    private static void buildHeapFile(File source_File, File target_File, int parseInt) {
+        
+    }
 }

3bcf83dc10c9470e63195e75185b41234cf9a9a9
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-25 20:14:54 +0900

    DBs1

--- src/dbquery.java
+++ src/dbquery.java
@@ -15,7 +15,17 @@ public class dbquery {
         search(heapFile,Integer.parseInt(args[1]),String.valueOf(args[0]));
     }
 
-    private static void search(File heapFile, int parseInt, String valueOf) {
-        
+    private static void search(File heapFile, int pagesize, String search_text) {
+        int rec_size = 0;
+        long s_Time = System.currentTimeMillis();
+        int[] field_size = { 4, 24, 24, 16, 10, 40, 20, 4, 32, 32, 32, 4, 8 };
+        for (int i = 0; i < field_size.length ; i++) {
+            rec_size += field_size[i];
+        }
+        int rec_num = (pagesize * 8) /  (rec_size * 8 + 1);  //floor comes for free
+        int cur_page = 0;
+        int remainsize = pagesize % rec_size;
+        int total_rec = 0;
+        int total_reresults = 0;
     }
 }

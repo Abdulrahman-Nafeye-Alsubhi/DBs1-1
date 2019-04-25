72cfcf43922af4463ba28f0c23e94cc4414e6401
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-25 19:19:53 +0900

    DBs1

--- src/dbquery.java
+++ src/dbquery.java
@@ -1,13 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Ashley
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
 public class dbquery {
-    
+    public static void main (String args[])
+            throws IOException, FileNotFoundException, Exception {
+        if (args.length!=2){  //check parameter
+            System.err.println("Please correct parameter");
+            return;
+        }
+        File heapFile=new File("heap." + args[1]);
+        search(heapFile,Integer.parseInt(args[1]),String.valueOf(args[0]));
+    }
+
+    private static void search(File heapFile, int parseInt, String valueOf) {
+        
+    }
 }

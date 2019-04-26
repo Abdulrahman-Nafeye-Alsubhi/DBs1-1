c6cdedba2c81e5b7dffc69a7ea56fca8c6a4dc4d
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-26 20:55:06 +0900

    DBs1

---
+++ src/Reference.java
@@ -0,0 +1,12 @@
+
+class Reference {
+
+    boolean getMatch() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Node getNode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

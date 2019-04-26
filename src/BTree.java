21348b5fb856a91f54b8409af45676096bdf8ad4
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-26 19:49:14 +0900

    DBs1

--- src/BTree.java
+++ src/BTree.java
@@ -1,4 +1,17 @@
 
 public class BTree {
+    Node root;
     
+    int degree;
+    
+    private static int nodeNames = 1;
+    
+    /**
+   Construct a BTree object with degree d. 
+   @param d the degree of BTree
+     */
+    public BTree (int d) {
+        degree = d;
+        root = null;
+    }
 }

b5c95b0346acb8356a275497d046eea9984a06bc
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-26 20:28:10 +0900

    DBs1

--- src/BTree.java
+++ src/BTree.java
@@ -15,7 +15,25 @@ public class BTree {
         root = null;
     }
 
-    void insert(String index_key) {
-        
+    void insert(String val) {
+        if (root == null) {
+                // This is the first insert in the tree - time to create a node
+                root = new LeafNode  (degree, val, null, null);
+        } else {
+            // First search where to put the new key
+            Reference l = root.search (val);
+            if (l.getMatch ()){
+                //System.out.println(val + " is already in the tree.");
+            }
+            else {
+                // The tree doesn't contain this key so call insert on the leaf where it should appear
+
+                l.getNode ().insert (val, null);
+
+                // Check to see if we have a new root and update if necessary
+                if (root.getParent () != null)
+                        root = root.getParent ().getNode ();
+            }
+        }
     }
 }

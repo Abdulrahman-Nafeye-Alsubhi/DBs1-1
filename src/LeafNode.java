a43fb8d6cd58e6284790078c06729b365921a5c9
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-27 21:40:42 +0900

    DBs1

--- src/LeafNode.java
+++ src/LeafNode.java
@@ -1,7 +1,16 @@
 
 class LeafNode extends Node {
 
-    LeafNode(int degree, String val, Object object, Object object0) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    /**
+    Construct a LeafNode object and initialize it with the parameters.
+    @param d the degree of the leafnode
+    @param k the first key value of the node
+    @param n the next node 
+    @param p the previous node
+      */
+     public LeafNode (int d, String k, Node n, Node p){
+             super (d, n, p);
+             keys [1] = k;
+             lastindex = 1;
+     }
 }

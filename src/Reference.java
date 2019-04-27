63e3b1211594fcfdedaa340acb2ad38138af23e4
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-27 22:40:47 +0900

    DBs1

--- src/Reference.java
+++ src/Reference.java
@@ -1,5 +1,25 @@
 
 class Reference {
+    
+    private Node node;                // What node is the referencing node?
+    private int index;                 
+    // What is the pointer or key index within the
+    //  referencing node?
+    private boolean match;               
+    // If this is returned as a result of a
+    //  search, was there a successful match?
+
+    /**
+   Constructs a Reference object and initializes the fields with the parameters.
+   @param p the node it refers
+   @param i the index of the key (pointer) it refers to 
+   @param m for the result of search, true if the key matchs the search value
+     */
+    public Reference (Node p, int i, boolean m){
+            node = p;
+            index = i;
+            match = m;
+    }
 
     boolean getMatch() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.

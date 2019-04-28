74d39dc81e35407908f9e29177cf3d4d381920ce
Abdulrahman-Nafeye-Alsubhi <s3578164@student.rmit.edu.au>
2019-04-29 08:00:30 +0900

    DBs1

--- src/Node.java
+++ src/Node.java
@@ -57,9 +57,15 @@ public abstract class Node {
      }
 
     public abstract void insert (String val, Node ptr);
-
-    void UnnecessaryMethod() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    protected void UnnecessaryMethod() {
+            // set keys to 0 so that tests pass.
+            int i = lastindex+1;
+            while (i<degree) {
+                    ptrs[i]=null;
+                    keys[i]="";
+                    i++;
+            }
     }
     
 }
